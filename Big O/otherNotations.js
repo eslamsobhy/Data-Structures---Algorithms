6d96eeaaeaf4ba16f8144100e2d60d7df9b9054c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-28 06:31:24 +0200

    implemented an O(n^2) problem

--- Big O/otherNotations.js
+++ Big O/otherNotations.js
@@ -12,3 +12,17 @@ const grabFirstTwoBoxes = (arr) => {
 grabFirstBox(boxes); // O(1) --> Constatnt Time
 
 grabFirstTwoBoxes(boxes); // O(2) --> STILL Constant Time
+
+// Write a function that will log all the pairs of this array, what is the complexity of it!?
+
+const letters = ["a", "b", "c", "d", "e"];
+
+const logAllpairs = (arr) => {
+  arr.forEach((letter1) => {
+    arr.forEach((letter2) => {
+      console.log(letter1, letter2);
+    });
+  });
+};
+
+logAllpairs(letters); // O(n * n) === O(n^2) --> quadratic time
