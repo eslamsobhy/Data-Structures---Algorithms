f7298b22ee0a884a423a033b5fb0d5cdb4d4edc9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-28 06:17:48 +0200

    presented the 3rd rule to calculate Big O: Different terms for each input!

--- Big O/rules.js
+++ Big O/rules.js
@@ -55,3 +55,21 @@ printFirstItemAndHalfOfTheItemsAndHi100Times(everyone); // O(n)
 // When the array gets bigger and bigger those 101 steps or even million of steps become very small to care about, so we neglect them!
 // Even dividing by 2 becomes not really important to care about!
 // So dividing, adding, multiplying, or subtracting (is there any!? :)!) are not considered while calculating!
+
+// 3. Different terms for different inputs
+
+const compressBoxes = (boxes1, boxes2) => {
+  // O(n)
+  boxes1.forEach((box) => {
+    console.log(box);
+  });
+
+  // O(m)
+  boxes2.forEach((box) => {
+    console.log(box);
+  });
+};
+
+// O(n + m)
+// Each input has different term from the other
+// If the two loops were nested, we'd have O(n*m)
