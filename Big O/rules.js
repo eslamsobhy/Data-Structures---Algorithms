c03d8a3b9ddbb3301df0c4cb585cdb84a20a306f
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-28 06:08:26 +0200

    presented the 2nd rule to calculate the Big O: Drop Constants!

--- Big O/rules.js
+++ Big O/rules.js
@@ -30,3 +30,28 @@ findNemo(everyone); // O(n) --> Linear time
 // It doesn't matter if we found nemo at the beginnig of our search or at the end of it!!
 // Big(O) will always treat it as it ran the worst case (at the end, or not even there!)
 // It will always has a Big O(n), no matter where did we find it!
+
+// 2. Remove (Drop) constants
+
+const printFirstItemAndHalfOfTheItemsAndHi100Times = (items) => {
+  console.log(items[0]);
+
+  const mid = Math.floor(items.length / 2);
+  let index = 0;
+
+  while (index < mid) {
+    console.log(items[index]);
+    index++;
+  }
+
+  for (let i = 0; i < 100; i++) {
+    console.log("Hi");
+  }
+};
+
+printFirstItemAndHalfOfTheItemsAndHi100Times(everyone); // O(n)
+
+// O(1 + n/2 + 100) == O(n/2 + 101) == O(n/2) == O(n)
+// When the array gets bigger and bigger those 101 steps or even million of steps become very small to care about, so we neglect them!
+// Even dividing by 2 becomes not really important to care about!
+// So dividing, adding, multiplying, or subtracting (is there any!? :)!) are not considered while calculating!
