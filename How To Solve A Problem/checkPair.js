c6c869b08891f10e696179756a25e56486ed1187
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-01 19:14:31 +0200

    Faster solution

--- How To Solve A Problem/checkPair.js
+++ How To Solve A Problem/checkPair.js
@@ -12,6 +12,11 @@ let answer = {
   pair: [],
 };
 
+let answerFast = {
+  status: false,
+  pair: [],
+};
+
 // The naive soln:
 const checkPair = (arr, s) => {
   for (let i = 0; i < arr.length; i++) {
@@ -27,4 +32,35 @@ const checkPair = (arr, s) => {
   return answer;
 };
 
+/*
+    In this solution we assume that the array is sorted!
+    if not sorted, we sort it; then it wil have a O(n*log n)
+*/
+
+// Faster soln:
+const checkPairFast = (arr, s) => {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low < high) {
+    let sum = arr[low] + arr[high];
+    // FOUND!
+    if (sum === s) {
+      answerFast.status = true;
+      answerFast.pair.push(arr[low]);
+      answerFast.pair.push(arr[high]);
+      return answerFast;
+    }
+    // we need to go lower
+    if (sum > s) {
+      high--;
+    }
+    // we need to go higher
+    if (sum < s) {
+      low++;
+    }
+  }
+  return answerFast;
+};
+
 console.log(checkPair(sample2, sum)); //O(n^2) quadratic (slow)
+console.log(checkPairFast(sample1, sum)); //O(n) linear (fast)
