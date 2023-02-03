13cbf5b8f348f567dfde1899a60503f9ad87704a
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-03 18:36:34 +0200

    implemented faster solution

--- How To Solve A Problem/commonItems.js
+++ How To Solve A Problem/commonItems.js
@@ -15,7 +15,7 @@
 // two parameters as inputs (arrays with no size limit)
 // gonna return true or false
 
-// The easy naive solution: O(n^2) you do not have to code, you can just talk about it!
+// The easy naive solution: O(n*m) you do not have to code, you can just talk about it!
 
 const firstArr = ["a", "b", "c", "x"];
 const secondArr = ["y", "z", "x"];
@@ -33,3 +33,45 @@ const isThereCommonItems = (arr1, arr2) => {
 };
 
 console.log(isThereCommonItems(firstArr, secondArr));
+
+// Another way of doing it:
+/*
+    There is a common pattern that is used in order to optimize the ou solution:
+    - convert the first array into an object like this:
+    obj = {
+        a: true,
+        b: true,
+        c: true,
+        x: true,
+    }
+    - then we check if the item in the second array is a property inside of the object:
+    secondArr[index] === obj.properties??
+*/
+
+const containsCommonItems = (arr1, arr2) => {
+  /*
+    Loop through the first array and create an object
+    whose properties are the array items...
+  */
+  let map = {};
+  arr1.forEach((item) => {
+    if (!map[item]) {
+      map[item] = true; // map={whatever_item: true}
+    }
+  });
+
+  /*
+    Loop through the second array and check if the items
+    inside of it exists in the created objects...
+  */
+  let flag = false;
+  arr2.forEach((item) => {
+    if (map[item]) {
+      flag = true;
+    }
+  });
+
+  return flag;
+};
+
+console.log(containsCommonItems(firstArr, secondArr)); // O(n + m) which is much faster - Linear
