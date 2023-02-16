bbc93ee93e5674b0b9b319acbea6e858292040d9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-16 15:48:47 +0200

    took advantage of arrays built in methods

--- Data Structures/1-Arrays/reverseString.js
+++ Data Structures/1-Arrays/reverseString.js
@@ -10,5 +10,22 @@ const reverseString = (str) => {
   return newStr;
 };
 
+// converting the string to an array to make use of its built in methods
+const reverse2 = (str) => {
+  // error checks
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "hmmm, not a good input actually!";
+  }
+
+  const backWard = [];
+  const totalItems = str.length - 1;
+
+  for (let i = totalItems; i >= 0; i--) {
+    backWard.push(str[i]);
+  }
+
+  return backWard.join(""); // converting it back to a string
+};
+
 console.log(reverseString("Hi my name is eslam!"));
-console.log(reverseString("1234"));
+console.log(reverse2("Hi my name is eslam!"));
