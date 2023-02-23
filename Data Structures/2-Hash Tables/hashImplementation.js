4eed7f0c3d488ee8134536b6bad45438f7e3db53
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-23 20:59:25 +0200

    implemented the hash function

---
+++ Data Structures/2-Hash Tables/hashImplementation.js
@@ -0,0 +1,17 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  // hash function
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
+
+const hashObj = new HashTable(6);
+console.log(hashObj._hash("grabes"));
