441b155f27f44cedaed9c4094eefd2f3d342e03d
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-24 18:42:25 +0200

    implemented inserting to the hash table

--- Data Structures/2-Hash Tables/hashImplementation.js
+++ Data Structures/2-Hash Tables/hashImplementation.js
@@ -11,7 +11,22 @@ class HashTable {
     }
     return hash;
   }
+
+  // adding to the hash table
+  set(key, value) {
+    const index = this._hash(key);
+    this.data[index] = new Array();
+    const obj = { k: key, v: value };
+    this.data[index].push(obj);
+    return this.data[index];
+  }
 }
 
-const hashObj = new HashTable(6);
+const hashObj = new HashTable(50);
 console.log(hashObj._hash("grabes"));
+console.log(hashObj.set("grabes", 1000));
+console.log(hashObj.set("grabes", 100));
+console.log(hashObj.set("grabes", 200));
+console.log(hashObj._hash("apples"));
+console.log(hashObj.set("apples", 430));
+console.log(hashObj);
