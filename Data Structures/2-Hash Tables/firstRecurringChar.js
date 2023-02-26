e1aa597cb6ed5d072a060ec99422e90f59c402eb
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-26 17:44:42 +0200

    calculated the first recurring element (naive implementation)

--- Data Structures/2-Hash Tables/firstRecurringChar.js
+++ Data Structures/2-Hash Tables/firstRecurringChar.js
@@ -45,10 +45,51 @@ class HashTable {
       }
     }
   }
+
+  //   the number with the minimum distance between its first and second occurrences
+  _min(array) {
+    let min = array[0];
+    array.forEach((element) => {
+      if (element.c < min.c) {
+        min = element;
+      }
+    });
+    return min;
+  }
+
+  //   first recurring element => O(n * m)
+  firstRecurringElement() {
+    const recurring = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] && this.data[i].secondOccur) {
+        let indexCount = 0;
+        for (
+          let j = this.data[i].firstOccur;
+          j <= this.data[i].secondOccur;
+          j++
+        ) {
+          indexCount += j;
+        }
+        let obj = { num: this.data[i].num, c: indexCount };
+        recurring.push(obj);
+      }
+    }
+    return this._min(recurring);
+  }
 }
 
-const arr1 = [2, 1, 1, 2, 3, 5, 1, 2, 4, 5];
+const arr1 = [2, 1, 1, 2, 3, 5, 1, 2, 4, 5]; // 1
+const arr2 = [2, 5, 1, 2, 3, 5, 1, 2, 4]; // 2
+const arr3 = [1, 2, 3, 5]; // undefined
 
-const hashObj = new HashTable(20);
-hashObj.fillHashTable(arr1);
-console.log(hashObj);
+const hashObj1 = new HashTable(20);
+const hashObj2 = new HashTable(20);
+const hashObj3 = new HashTable(20);
+
+hashObj1.fillHashTable(arr1);
+hashObj2.fillHashTable(arr2);
+hashObj3.fillHashTable(arr3);
+
+console.log(hashObj1.firstRecurringElement());
+console.log(hashObj2.firstRecurringElement());
+console.log(hashObj3.firstRecurringElement());
