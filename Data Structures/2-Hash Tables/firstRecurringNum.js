140dcc2574a24abb9097c6aa2c1492b9209e024f
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-28 17:12:34 +0200

    implemented a faster algorithm for the problem's v2

--- Data Structures/2-Hash Tables/firstRecurringNum.js
+++ Data Structures/2-Hash Tables/firstRecurringNum.js
@@ -48,9 +48,52 @@ const firstRecurringChar2 = (array) => {
   return undefined;
 };
 
+// This approach meets this version's requirements
+const firstRecurringChar3 = (array) => {
+  // using the built-in hash table
+  const map = {};
+
+  // array of repeated elements
+  const repeated = [];
+
+  // filling the hash-table with the array's elements indices
+  for (let i = 0; i < array.length; i++) {
+    if (map[array[i]]) {
+      map[array[i]].push(i);
+    } else {
+      map[array[i]] = [i];
+    }
+  }
+
+  // filling the array of repeated elements
+  for (const num in map) {
+    if (map[num].length > 1) {
+      let numData = { num, firstOccurance: map[num][0] };
+      repeated.push(numData);
+    }
+  }
+
+  // Determining the element that appeard first and then repeated
+  let firstRecurring = repeated[0];
+  for (let i = 1; i < repeated.length; i++) {
+    if (repeated[i].firstOccurance < firstRecurring.firstOccurance) {
+      firstRecurring = repeated[i];
+    }
+  }
+
+  // checking if there were no repeated elements in the input array
+  if (firstRecurring) {
+    return firstRecurring.num;
+  }
+
+  return undefined;
+};
+
 const arr1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
 const arr2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
 const arr3 = [1, 2, 3, 5];
 
-console.log(firstRecurringChar(arr2));
-console.log(firstRecurringChar2(arr2));
+// console.log(firstRecurringChar(arr2));
+// console.log(firstRecurringChar2(arr2));
+
+console.log("new function returns: ", firstRecurringChar3(arr3));
