2581a38801aa6446849fd699a7e897aedbc2abe9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-05 22:44:06 +0200

    hard coded our first linked list

---
+++ Data Structures/3-Linked Lists/linkedListImplementation.js
@@ -0,0 +1,16 @@
+// 10 -> 5 -> 16
+
+const myLinkedList = {
+  head: {
+    value: 10,
+    next: {
+      value: 5,
+      next: {
+        value: 16,
+        next: null,
+      },
+    },
+  },
+};
+
+console.log(myLinkedList);
